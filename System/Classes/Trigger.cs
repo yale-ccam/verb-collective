c632f41ec07103c42dc215054ae3fe0bd51131e4
YaleCCAM <52501309+YaleCCAM@users.noreply.github.com>
2019-08-08 01:04:16 -0400

    Add files via upload

---
+++ System/Classes/Trigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : Verb {
+
+    public override void Conjugate()
+    {
+
+    }
+}
---
+++ System/Classes/Verb.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Verb : MonoBehaviour {
+
+	public AudioClip verbSound;
+	public bool loopSound;
+	public bool isActive;
+
+	[HideInInspector]
+	public AudioSource audioS;
+
+	public void SetAudio () {
+		
+		if(gameObject.GetComponent<AudioSource>() == null)
+		{
+			gameObject.AddComponent<AudioSource>();
+		}
+
+		audioS = gameObject.GetComponent<AudioSource>();
+		audioS.clip = verbSound;
+		audioS.loop = loopSound;
+		/*if(isActive && !audioS.isPlaying){
+			audioS.Play();
+		}*/
+	}
+
+	public virtual void Conjugate(){
+        PlayAudio();
+	}
+
+    public void PlayAudio()
+    {
+        if (!audioS.isPlaying && (verbSound != null))
+        {
+            audioS.clip = verbSound;
+            audioS.loop = loopSound;
+            audioS.Play();
+        }
+    }
+
+    public void CeaseAudio()
+    {
+        if(audioS.clip == verbSound)
+        {
+            audioS.Stop();
+        }
+    }
+
+    public void Activate(Verb[] verbs)
+    {
+        foreach (Verb item in verbs)
+        {
+            if (item)
+            {
+                item.isActive = true;
+                item.Conjugate();
+            }
+        }
+    }
+
+    public void Deactivate(Verb[] verbs)
+    {
+        foreach (Verb item in verbs)
+        {
+            if (item)
+            {
+                item.isActive = false;
+            }
+        }
+    }
+
+    public virtual void EndVerb(){
+        isActive = false;
+        CeaseAudio();
+	}
+}
