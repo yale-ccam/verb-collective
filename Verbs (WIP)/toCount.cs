995257b12098f4aa7581b798a52711c411604685
BXB2 <34118361+BXB2@users.noreply.github.com>
2019-08-16 16:52:03 -0400

    Added a verb
    To Count is added

---
+++ Verbs (WIP)/toCount.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toCount : Verb
+{
+    public float rate = 1.0f;
+	public float targetNumber = 5.0f;
+	public Verb[] triggeredVerbs;
+
+	[SerializeField]
+	private float currentNumber = 0.0f;
+
+	void Awake () 
+	{
+		SetAudio();
+	}
+
+    private void Start()
+    {
+        if (isActive)
+            PlayAudio();
+    }
+
+    void FixedUpdate () {
+		if(isActive)
+		{
+			
+			currentNumber += rate;
+
+			if(currentNumber >= targetNumber)
+				{
+				currentNumber = 0.0f;
+				EndVerb();
+                Activate(triggeredVerbs);
+				}
+			else{
+				isActive = false;
+				}
+		}
+	}
+}
